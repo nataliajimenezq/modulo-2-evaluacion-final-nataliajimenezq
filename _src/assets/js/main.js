dc5ea043b54afe5dec45f35ca28d7f9727ba9b84
nataliajimenezq <snjimenezq@gmail.com>
2019-12-03 20:38:54 +0100

    Final fixes

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -8,7 +8,6 @@ const searchInput = document.querySelector('#search-input');
 //const favouriteList = document.querySelector('.favourites');
 const favouriteSeries = document.querySelector('.favourite__series');
 
-let elementsListSeries;
 
 //Creo un array vacío para guardar los favoritos
 let favouriteSeriesArray = [];
@@ -21,49 +20,41 @@ const imgDefault = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
 const connectToAPI = () => {
   fetch(urlBase + `${searchInput.value}`)
     .then(response => response.json())
-    .then(dataFromResponse => {
-      displaySeries(dataFromResponse)
-      elementsListSeries = document.querySelectorAll('.list__serie');
-    })
+    .then(dataFromResponse => displaySeries(dataFromResponse))
     .catch(error => console.log(error))
 }
-
+//funcion para leer mi localStorage por primera vez, si está vacío
 function loadFavouriteHandler() {
-  //Leo mi localstorage la primera vez
   const mylocalStorage = localStorage.getItem("favSeries");
-  //esta vacío mi localstorage?
-  if(mylocalStorage !== null) {
-    favouriteSeriesArray= JSON.parse(mylocalStorage);
+  if (mylocalStorage !== null) {
+    favouriteSeriesArray = JSON.parse(mylocalStorage);
   }
   paintFavourites(favouriteSeriesArray);
-
 }
+
 //función para guardar mi array de favoritos en el localStorage
 function setLocalStorage() {
   localStorage.setItem("favSeries", JSON.stringify(favouriteSeriesArray));
 }
-
 //handle para conectar a la api y pintar las series (función que agregamos como listener del boton)
 function searchSeriesHandler() {
   connectToAPI(urlBase.toLowerCase(), displaySeries);
 }
-
 //función para buscar dando enter en el buscador
-function enterKeyHandle(event){
+function enterKeyHandle(event) {
   event.preventDefault();
   searchSeriesHandler();
 }
 //funcion para pintar las series 
 const displaySeries = (seriesArray) => {
   for (const serie of seriesArray) {
-    const series = serie.show;
-    const seriesName = series.name;
-    const seriesImg = series.image;
+    const seriesName = serie.show.name;
+    const seriesImg = serie.show.image;
 
     const elementLi = document.createElement('li');
     const imgElement = document.createElement('img');
     const spanElement = document.createElement('span');
-    
+
     const spanContent = document.createTextNode(seriesName);
     elementLi.classList.add('serie__item');
     imgElement.classList.add('serie__img');
@@ -81,34 +72,38 @@ const displaySeries = (seriesArray) => {
     }
   }
 }
-
 //función para añadir a favoritos //eventcurrent del input
+//Intento de borrar del array de favoritos al hacer click
 
 function addFavourite(event) {
   let favouriteSerie = event.currentTarget;
   favouriteSerie.classList.toggle('selected');
   const favoriteImage = favouriteSerie.querySelector('.serie__img');
   const favoriteName = favouriteSerie.querySelector('.serie__tittle');
-  
+
   const favInfo = {
     name: favoriteName.innerHTML,
-    image:favoriteImage.src
+    image: favoriteImage.src
   };
+
   if (favouriteSerie.classList.contains('selected')) {
     favouriteSeriesArray.push(favInfo);
     paintFavourites();
     setLocalStorage();
+  } else{
+    for(let i=0;i<favouriteSeriesArray.length;i++){
+      if(favouriteSeriesArray[i].name===favoriteName.innerHTML){
+        favouriteSeriesArray.splice(i,1);
+      }
+    }  
   }
+  setLocalStorage();
+}
+ //función para esconder los favoritos (visualmente)
+function deleteFromFav(event){
+  event.currentTarget.closest('li').classList.add('hidden');
 }
 
-//función para eliminar de favoritos //EN PROCESO
-const deleteFromFav=()=>{
-  for (let i = 0; i < favouriteSeriesArray.length; i++) {
-    if (favouriteSeriesArray.name===favInfo.name) {
-      favouriteSeriesArray.splice(i, 1);
-    }
-}
-}
 //función para pintar los favoritos
 function paintFavourites() {
   favouriteSeries.innerHTML = '';
@@ -119,8 +114,8 @@ function paintFavourites() {
     const favElementList = document.createElement('li');
     const favElementSpan = document.createElement('span');
     const favElementImg = document.createElement('img');
-    const favElementDel =document.createElement('div');
-    
+    const favElementDel = document.createElement('div');
+
     const spanContent = document.createTextNode(favouriteName);
 
     favElementSpan.appendChild(spanContent);
@@ -128,18 +123,16 @@ function paintFavourites() {
     favElementList.appendChild(favElementSpan);
     favElementList.appendChild(favElementDel);
     favouriteSeries.appendChild(favElementList);
-    
+
     favElementImg.src = favouriteImg;
     favElementList.classList.add('favourite__item');
     favElementImg.classList.add('favourite__item-img');
     favElementDel.classList.add('favourite__item-del');
-    favElementDel.addEventListener('click',deleteFromFav);
+    favElementDel.addEventListener('click', deleteFromFav);
   }
-
 }
 
-
 buttonSearch.addEventListener('click', searchSeriesHandler);
-elementForm.addEventListener('submit',enterKeyHandle);
-window.addEventListener('load',loadFavouriteHandler);
+elementForm.addEventListener('submit', enterKeyHandle);
+window.addEventListener('load', loadFavouriteHandler);
 
